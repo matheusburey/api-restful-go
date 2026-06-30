f0591f265c7c006c97771be9e873ee540cd75c95
matheusburey <matheusbureidev@gmail.com>
2026-06-30 10:23:08 -0300

    feat: update user model and queries to include password and bio fields

--- internal/store/pgstore/migrations/001_create_users_table.sql
+++ internal/store/pgstore/migrations/001_create_users_table.sql
@@ -1,11 +1,13 @@
 -- This is a sample migration.
 CREATE TABLE IF NOT EXISTS users (
     id UUID primary key NOT NULL DEFAULT gen_random_uuid(),
-    name VARCHAR NOT NULL,
-    email VARCHAR UNIQUE NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP NOT NULL DEFAULT now()
+    name VARCHAR(80) UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash BYTEA NOT NULL,
+    bio TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
   );
 
 ---- create above / drop below ----
-DROP TABLE users;
+DROP TABLE IF EXISTS users;
--- internal/store/pgstore/queries/users.sql
+++ internal/store/pgstore/queries/users.sql
@@ -1,6 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO users ("name", "email")
-    VALUES ($1, $2)
+INSERT INTO users ("name", "email", "password_hash", "bio")
+    VALUES ($1, $2 , $3, $4)
 RETURNING id;
 
 -- name: GetUserById :one
@@ -11,7 +11,7 @@ SELECT * FROM users;
 
 -- name: UpdateUser :one
 UPDATE users
-    SET "name" = $2, "email" = $3, updated_at = now()
+    SET "name" = $2, "email" = $3, "bio" = $4, updated_at = now()
     WHERE id = $1
 RETURNING *;
 
